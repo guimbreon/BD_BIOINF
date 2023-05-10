694d7848e9a217fd8d5a1f03f83a7f2a8647d0e2
Guimbreon <gui.acs03@gmail.com>
2023-05-10 15:04:46 +0100

    Little changes into the script

--- Entidades.sql
+++ Entidades.sql
@@ -1,9 +1,7 @@
 USE T2_G3
 
-Using_Schema = [202200596]
-
 -- Requerente
-CREATE TABLE Using_Schema.Requerente (
+CREATE TABLE [202200596].Requerente (
     ID_Requerente INT PRIMARY KEY,
     Nome VARCHAR(255),
     ID_Categoria INT,
@@ -18,43 +16,43 @@ CREATE TABLE Using_Schema.Requerente (
 );
 
 -- Destino
-CREATE TABLE Using_Schema.Destino (
+CREATE TABLE [202200596].Destino (
     ID_Destino INT PRIMARY KEY,
     Destino VARCHAR(255)
 );
 
 -- Categoria
-CREATE TABLE Using_Schema.Categoria (
+CREATE TABLE [202200596].Categoria (
     ID_Categoria INT PRIMARY KEY,
     Categoria VARCHAR(255)
 );
 
 -- Data_inicio
-CREATE TABLE Using_Schema.Data_inicio (
+CREATE TABLE [202200596].Data_inicio (
     ID_DataInicio INT PRIMARY KEY,
     Data_Inicio DATE
 );
 
 -- Data_fim
-CREATE TABLE Using_Schema.Data_fim (
+CREATE TABLE [202200596].Data_fim (
     ID_DataFim INT PRIMARY KEY,
     Data_fim DATE
 );
 
 -- Motivo
-CREATE TABLE Using_Schema.Motivo (
+CREATE TABLE [202200596].Motivo (
     ID_Motivo INT PRIMARY KEY,
     Motivo VARCHAR(255)
 );
 
 -- Serviço_docente
-CREATE TABLE Using_Schema.Servico_docente (
+CREATE TABLE [202200596].Servico_docente (
     ID_ServicoDocente INT PRIMARY KEY,
     Servico_docente VARCHAR(255)
 );
 
 -- Encargos
-CREATE TABLE Using_Schema.Encargos (
+CREATE TABLE [202200596].Encargos (
     ID_Encargos INT PRIMARY KEY,
     ID_EntidadeConvida INT,
     ID_FP INT,
@@ -63,37 +61,37 @@ CREATE TABLE Using_Schema.Encargos (
 );
 
 -- Entidade_Convida
-CREATE TABLE Using_Schema.Entidade_Convida (
+CREATE TABLE [202200596].Entidade_Convida (
     ID_EntidadeConvida INT PRIMARY KEY,
     Nome_Entidade_Convida VARCHAR(255)
 );
 
 -- FP
-CREATE TABLE Using_Schema.FP (
+CREATE TABLE [202200596].FP (
     ID_FP INT PRIMARY KEY,
     FP VARCHAR(255)
 );
 
 -- Entidade_Externa
-CREATE TABLE Using_Schema.Entidade_Externa (
+CREATE TABLE [202200596].Entidade_Externa (
     ID_EntidadeExterna INT PRIMARY KEY,
     Entidade_Externa VARCHAR(255)
 );
 
 -- Serviço
-CREATE TABLE Using_Schema.Servico (
+CREATE TABLE [202200596].Servico (
     ID_Servico INT PRIMARY KEY,
     Servico VARCHAR(255)
 );
 
 -- Objetivo
-CREATE TABLE Using_Schema.Objetivo (
+CREATE TABLE [202200596].Objetivo (
     ID_Objetivo INT PRIMARY KEY,
     Objetivo VARCHAR(255)
 );
 
 -- EstimaDespesa
-CREATE TABLE Using_Schema.EstimaDespesa (
+CREATE TABLE [202200596].EstimaDespesa (
     ID_EstimaDespesa INT PRIMARY KEY,
     Inscrição VARCHAR(255),
     ID_Refeicoes INT,
@@ -101,7 +99,7 @@ CREATE TABLE Using_Schema.EstimaDespesa (
 );
 
 -- Refeicoes
-CREATE TABLE Using_Schema.Refeicoes (
+CREATE TABLE [202200596].Refeicoes (
     ID_Refeicoes INT PRIMARY KEY,
     ID_EstimaDespesa INT,
     Almoco VARCHAR(255),
@@ -109,13 +107,13 @@ CREATE TABLE Using_Schema.Refeicoes (
 );
 
 -- Transportes
-CREATE TABLE Using_Schema.Transportes (
+CREATE TABLE [202200596].Transportes (
     ID_Transportes INT PRIMARY KEY,
     Tipo_Transporte VARCHAR(255)
 );
 
 --DocumentosAnexo
-CREATE TABLE Using_Schema.DocumentosAnexo (
+CREATE TABLE [202200596].DocumentosAnexo (
     ID_DocumentoAnexo INT PRIMARY KEY,
     DocumentoAnexo VARCHAR(255)
 );
